d4fc54ec052f8e30adead20767699c507df8234b
Mats Grimsgaard <mats@mgv.no>
2014-07-16 08:57:47 +0200

    Mindre endringer i hjelpetekst.js

--- demo/hjelpetekst.js
+++ demo/hjelpetekst.js
@@ -9,7 +9,7 @@
 		xmlhttp.onreadystatechange  = function(){
 			if (xmlhttp.readyState == 4 && xmlhttp.status == 200 ){
 			    var stilark = document.createElement('style');
-				stilark.innerHTML = "a.hjelpetekst:before{content: \"?\";margin-left: 3px;width: 15px;height: 16px;margin-top: -8px;font-size: 14px;font-weight: bold;line-height: 15px;text-align: center;color: #000;background: #FCE300;-webkit-border-radius: 16px;-moz-border-radius: 16px;border-radius: 16px;display:inline-block;border: 1px solid #000;font-family:arial;padding-right:1px;-webkit-transition-duration: .5s;transition-duration: .5s;}a.hjelpetekst{cursor:pointer;}a.hjelpetekst:hover:before{background-color:orange;}#hjelpetekst_overlay{background:rgba(0,0,0,0.4);display:block;}#hjelpetekst_overlay,#hjelpetekst_dialog{position:fixed;z-index:1000;top:0;left:0;width:100%;height:100%;visibility:hidden;}#hjelpetekst_dialog{display:table;}#hjelpetekst_dialog div{display:table-cell;vertical-align:middle;}#hjelpetekst_dialog div div{width:280px;margin:auto;background-color:#FCE300;font-size:15px;line-height:1.4;padding: 15px;border: 3px solid #000;display:block;z-index:10001;}#hjelpetekst_dialog h3{margin:0;border-bottom: 1px solid #000;padding-bottom: 6px;}#hjelpetekst_dialog h3 a{float:right;margin-left: 4px;}#hjelpetekst_dialog a{text-decoration:none;border: 1px solid #000;background:#fff; padding: 2px 10px; margin: -2px 0; display: inline-block; text-align: center; font-weight: bold; }#hjelpetekst_dialog p, #hjelpetekst_dialog iframe { margin: 10px 0 15px; padding-bottom: 10px; border-bottom: 1px solid #000; height: 210px; }";
+				stilark.innerHTML = "a.hjelpetekst{-moz-border-radius:16px;-webkit-border-radius:16px;-webkit-transition-duration:.5s;background:#FCE300;border:1px solid #000;border-radius:16px;color:#000;cursor:pointer;display:inline-block;font-family:arial;font-size:14px;font-weight:700;height:16px;line-height:15px;margin-left:3px;margin-top:-8px;text-align:center;text-decoration:none;transition-duration:.5s;width:16px;}a.hjelpetekst:hover{background-color:orange;}#hjelpetekst_overlay{background:rgba(0,0,0,0.4);display:block;}#hjelpetekst_overlay,#hjelpetekst_dialog{height:100%;left:0;position:fixed;top:0;visibility:hidden;width:100%;z-index:1000;}#hjelpetekst_dialog{display:table;}#hjelpetekst_dialog div{display:table-cell;vertical-align:middle;}#hjelpetekst_dialog div div{background-color:#FCE300;border:3px solid #000;display:block;font-size:15px;line-height:1.4;margin:auto;padding:15px;width:280px;z-index:10001;}#hjelpetekst_dialog h3{border-bottom:1px solid #000;margin:0;padding-bottom:6px;}#hjelpetekst_dialog h3 a{float:right;margin-left:4px;}#hjelpetekst_dialog a{background:#fff;border:1px solid #000;display:inline-block;font-weight:700;margin:-2px 0;padding:2px 10px;text-align:center;text-decoration:none;}#hjelpetekst_dialog p,#hjelpetekst_dialog iframe{border-bottom:1px solid #000;height:210px;margin:10px 0 15px;padding-bottom:10px;}";
 				document.body.appendChild(stilark);
 				var overlay = document.createElement('div');
 				overlay.id = "hjelpetekst_overlay";
@@ -27,10 +27,15 @@
 						var o = document.getElementById(h.id);
 						if(o) {
 							var t = document.createElement('a');
+							t.innerHTML = "?";
 							t.className = 'hjelpetekst';
-							t.href = "javascript:hjelpetekst('" + san(h.tittel) + "', '" + san(h.tekst) + "', '" + encodeURI(h.video) + "','" + encodeURI(h.link) + "')";
+							t.onclick = hjelpetekst(h.tittel, h.tekst, encodeURI(h.video), encodeURI(h.link));
+							//t.href = "javascript:hjelpetekst('" + san(h.tittel) + "', '" + san(h.tekst) + "', '" + encodeURI(h.video) + "','" + encodeURI(h.link) + "')";
 							t.title = h.tittel + ' (Klikk for hjelp)';
 							o.parentNode.insertBefore(t, o.nextSibbling);
+						}
+						else {
+							console.log("Elementet '" + h.id + "' finnes ikke i denne visningen, og bør fjernes fra dokumentet.");
 						}
 					}
 				}
@@ -44,7 +49,9 @@
 			var btns = '';
 			if (v) { btns = '<a href="javascript:hjelpetekstVideo(\'' + v + '\')" style="width: 110px">Video</a> '; }
 			if (m) { btns = '<a href="' + m + '" target="_blank" style="width: 110px;float:right;">Les mer</a>' + btns; }
-			d.innerHTML = "<div><div><h3><a href=\"javascript:hjelpetekstHide();\">X</a> " + t + "</h3><p>" + i + "</p>" + btns + "</div></div>";
+			d.innerHTML = "<div><div><h3><a href=\"javascript:hjelpetekstHide();\">X</a><span></span></h3><p></p>" + btns + "</div></div>";
+			document.getElementById("hjelpetekst_dialog").getElementsByTagName("span")[0].textContent = t;
+			document.getElementById("hjelpetekst_dialog").getElementsByTagName("p")[0].textContent = i;
 			o.style.visibility = "visible";
 			d.style.visibility = "visible";
 		}
